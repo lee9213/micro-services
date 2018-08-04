a967e740f1abfb5cf112f024ff8a6e6bd0329c69
lee9213 <lee9213@163.com>
2018-08-04 23:43:39 +0800

    add config by git service

---
+++ spring-cloud-config-services/spring-cloud-config-git-service/src/main/java/com/lee9213/config/server/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.lee9213.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author lee9213@163.com
+ * @version 1.0
+ * @date 2018-08-01 21:54
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
