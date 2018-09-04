0ba745153d7455ce225c41f1966649901cd7c718
lee9213 <lee9213@163.com>
2018-09-04 17:20:12 +0800

    modify spring cloud config add config to eureka

--- spring-cloud-config-services/spring-cloud-config-git-service/src/main/java/com/lee9213/config/git/server/ConfigServerApplication.java
+++ spring-cloud-config-services/spring-cloud-config-git-service/src/main/java/com/lee9213/config/git/server/ConfigServerApplication.java
@@ -1,7 +1,8 @@
-package com.lee9213.config.server;
+package com.lee9213.config.git.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
