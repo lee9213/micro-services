4c373fd398e290978ca17a26fec97eeefbc73de2
lee9213 <lee9213@163.com>
2018-09-08 15:58:03 +0800

    modify structure

--- spring-cloud-multiple-services/spring-cloud-zookeeper-service/src/main/java/com/lee9213/config/zookeeper/server/ZookeeperServerApplication.java
+++ spring-cloud-multiple-services/spring-cloud-zookeeper-service/src/main/java/com/lee9213/config/zookeeper/server/ZookeeperServerApplication.java
@@ -11,9 +11,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ZookeeperConfigServerApplication {
+public class ZookeeperServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZookeeperConfigServerApplication.class, args);
+        SpringApplication.run(ZookeeperServerApplication.class, args);
     }
 }
